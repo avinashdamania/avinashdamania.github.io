d975989cb2303da8a1c45387c8caa8a5e93b3941
Avinash Damania - Intern <Avinash.Damania@bentley.com>
2018-05-18 11:29:59 -0500

    updated transitions for template injections on project bar

---
+++ README.md
@@ -0,0 +1,9 @@
+# my-website
+a small personal website
+
+to run:
+
+1. download the npm package http-server
+2. run a server from the root directory of the repo (not index.html!!)
+
+(otherwise, angularjs' cross origin requests will not be allowed and it won't work)
--- app.js
+++ app.js
@@ -1,23 +1,47 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ui.router', 'ngAnimate']);
 
 
-app.config(function($routeProvider) {
-	$routeProvider
-	.when("/", {
-		templateUrl: "templates/sourcreme.html",
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/default')
+	$urlRouterProvider.when('/default', '/default');
+	$stateProvider
+	.state('default', {
+		url: '/default',
+		templateUrl: 'templates/default.html',
+		controller: 'DefaultCtrl'
 	})
-	.when("/sourcreme", {
-		templateUrl: "templates/sourcreme.html",
+	.state('sourcreme', {
+		url: '/sourcreme',
+		templateUrl: 'templates/sourcreme.html',
+		controller: 'SourcremeCtrl'
 	})
-	.when("/robotnav", {
-		templateUrl: "templates/robotnav.html",
+	.state('/robotnav', {
+		url: '/robotnav',
+		templateUrl: 'templates/robotnav.html',
+		controller: 'RobotNavCtrl'
 	})
-	.when("/app4tht", {
-		templateUrl: "templates/app4tht.html"
+	.state('/app4tht', {
+		url: '/app4tht',
+		templateUrl: 'templates/app4tht.html',
+		controller: 'App4ThtCtrl'
 	})
-	.otherwise({redirectTo: "/"});
+	$urlRouterProvider.otherwise('/default');
+
 });
 
+app.controller('DefaultCtrl', function($scope) {
+	$scope.pageClass = 'default';
+})
 
+app.controller('SourcremeCtrl', function($scope) {
+	$scope.pageClass = 'sourcreme';
+})
 
+app.controller('RobotNavCtrl', function($scope) {
+	$scope.pageClass = 'robotnav';
+})
+
+app.controller('App4ThtCtrl', function($scope) {
+	$scope.pageClass = 'app4tht';
+})
 
